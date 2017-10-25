1beb3e146c85e6b29423cfbe26f60944131169d1
Malte Heithoff <Malte.Heithoff@rwth-aachen.de>
2017-10-25 11:46:52 +0200

    Try to SetUp Travis

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 count=$(find  -type d -name ".git" | wc -l)
 echo $count
 if [ "$count" -gt 0 ]
-echo $count
+    echo $count
+fi;
 #then
 ## if repos exist, then pull them
 #find -type d -name ".git" -exec sh -c 'cd $0 && cd .. && git pull' {} \;
 #else
 ## if workspace has been deleted clone
 #curl "https://api.github.com/orgs/EmbeddedMontiArc/repos?per_page=200" |  grep -e 'git_url*' | cut -d \" -f 4 |xargs -L1 git clone
-fi
+#fi;
