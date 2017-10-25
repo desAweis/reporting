bc32e39df05704a491d6e03d65b4b8c89bd3f5c4
Malte Heithoff <Malte.Heithoff@rwth-aachen.de>
2017-10-25 12:42:02 +0200

    Try to SetUp Travis

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
+mkdir out
+cd out
 count=$(find  -type d -name ".git" | wc -l)
-echo $count
 if [ "$count" -gt 0 ]
 then
     find -type d -name ".git" -exec sh -c 'cd $0 && cd .. && git pull' {} \
 else
     curl "https://api.github.com/orgs/EmbeddedMontiArc/repos?per_page=200" |  grep -e 'git_url*' | cut -d \" -f 4 |xargs -L1 git clone
-fi;
+fi
+cd ..;
