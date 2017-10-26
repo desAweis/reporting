bcde56b05aa3a1bddd5459b36711e47839f42213
Travis CI <travis@travis-ci.org>
2017-10-26 13:24:46 +0200

    Get it working

--- gitpull.sh
+++ gitpull.sh
@@ -13,4 +13,3 @@ else
 # if workspace has been deleted clone
 curl "https://api.github.com/orgs/EmbeddedMontiArc/repos?per_page=200" | grep -e 'git_url*' | cut -d \" -f 4 | xargs -L1 git clone
 fi
-cd..;
