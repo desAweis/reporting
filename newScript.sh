f71532b2ebf6b0462d19ecf86e6e4fc864e99fc3
Travis CI <travis@travis-ci.org>
2017-10-27 09:54:45 +0200

    Script broken?

--- newScript.sh
+++ newScript.sh
@@ -1,3 +1,6 @@
 mkdir projects
 cd projects
-curl "https://api.github.com/orgs/EmbeddedMontiArc/repos?per_page=200" | grep -e 'git_url*' | cut -d \" -f 4 | xargs -L1 git clone
+curl "https://api.github.com/orgs/EmbeddedMontiArc/repos?per_page=200" |
+   grep -e 'git_url*' |
+   cut -d \" -f 4 |
+   xargs -L1 git clone
