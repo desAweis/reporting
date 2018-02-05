9e809f8f7d5b2162110b16b1be9082d60df2bf57
Travis CI <travis@travis-ci.org>
2018-02-06 00:19:24 +0100

    Trying to fix travis

---
+++ scripts/gh_pages.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+
+cd ../report/data
+
+git add data.json
+git add dataEWT.json
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+chmod 600 github_deploy_key
+eval `ssh-agent -s`
+ssh-add gh-pages_key
+
+# Now that we're all set up, we can push.
+git push $SSH_REPO $TARGET_BRANCH
