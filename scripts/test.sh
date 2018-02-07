34689e4462be02d5a7cce8f3a2b2971aa2b68672
Travis CI <travis@travis-ci.org>
2018-02-07 20:53:57 +0100

    Deleted unnecessary files

--- scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ENCRYPTION_LABEL="f084573856bf"
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-eval `ssh-agent -s`
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in gh-pages_key.enc -d | ssh-add -
