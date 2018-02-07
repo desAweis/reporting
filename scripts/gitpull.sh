4e7e3b21fa0aa55543a67385840a75de2e8283eb
Travis CI <travis@travis-ci.org>
2018-02-07 19:36:53 +0100

    test gitpull script on travis

--- scripts/gitpull.sh
+++ scripts/gitpull.sh
@@ -4,19 +4,19 @@ then
 fi
 
 cd $1
-
-for entry in $(curl "https://api.github.com/orgs/$1/repos?per_page=200" | grep -e 'git_url*' | cut -d \" -f 4)
+repos=$(curl "https://api.github.com/orgs/$1/repos?per_page=200" | grep -e 'git_url*' | cut -d \" -f 4)
+for entry in $repos
 do
   url=$entry
   rep=$(basename $entry)
   rep="${rep%.*}"
-  if [ -d $rep ]
-  then
-    cd $rep && git pull && cd ..
-  elif [ $rep != "external-dependencies" ]
-  then
-    git clone "https://github.com/$1/$rep"
-  fi
+  echo $rep
+  # if [ -d $rep ]
+  # then
+    # cd $rep && git pull && cd ..
+  # elif [ $rep != "external-dependencies" ]
+  # then
+    # git clone "https://github.com/$1/$rep"
+  # fi
 done
-echo "test123"
 cd ..
