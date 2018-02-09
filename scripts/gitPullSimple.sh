bac14ac20575cb3c864a9744288eb8bfd592c9bb
Travis CI <travis@travis-ci.org>
2018-02-09 15:45:40 +0100

    Switching back to simple git pull script

--- scripts/gitPullSimple.sh
+++ scripts/gitPullSimple.sh
@@ -1,33 +1,25 @@
+#!/bin/bash
+
+declare -a emaRepos=( "Documentation" "EMA2Kotlin" "EMAM2Cpp" "EMAM2PythonROS" "EMAM2RosCpp" "EmbeddedMontiArc" "EmbeddedMontiArcMath" \
+                "MontiMath" "OCL" "OCL2Java" "Tagging" "Tagging-Examples" "ViewVerification" "visualisation" )
+declare -a msRepos=( "CarLang" "server" "visualization" "SimLang" "simulation" "commons" "application" "example-autopilot-ema" \
+                     "controller" "Documentation" )
+
 # clone EmbeddedMontiArc
 if [ ! -d "EmbeddedMontiArc" ]
 then
   mkdir "EmbeddedMontiArc"
 fi
 cd EmbeddedMontiArc
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/EmbeddedMontiArc" "EmbeddedMontiArc"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/EMAM2Cpp" "EMAM2Cpp"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/Struct" "Struct"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/EMA2Kotlin" "EMA2Kotlin"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/Documentation" "Documentation"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/EMAM2PythonROS" "EMAM2PythonROS"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/languagescommon" "languagescommon"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/ViewVerification" "ViewVerification"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/RewriteConf" "RewriteConf"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/OCL_EMA2Java" "OCL_EMA2Java"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/OCL2Java" "OCL2Java"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/OCL" "OCL"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/NumberUnit" "NumberUnit"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/EmbeddedMontiView" "EmbeddedMontiView"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/EmbeddedMontiArcMath" "EmbeddedMontiArcMath"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/EmbeddedMontiArcBehaviour" "EmbeddedMontiArcBehaviour"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/visualisation" "visualisation"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/MontiMath" "MontiMath"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/reporting" "reporting"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/demonstrator" "demonstrator"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/CNNTrainLang" "CNNTrainLang"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/CNNArchLang" "CNNArchLang"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/languages" "languages"
-git clone --depth 1 "https://github.com/EmbeddedMontiArc/ViewVerificationExamples" "ViewVerificationExamples"
+for repo in "${emaRepos[@]}"
+do
+  if [ -d $repo ]
+  then
+    cd $repo && git pull && cd ..
+  else
+    git clone --depth 1 "https://github.com/EmbeddedMontiArc/$repo"
+  fi
+done
 cd ..
 
 # clone MontiSim
@@ -35,15 +27,14 @@ if [ ! -d "MontiSim" ]
 then
   mkdir "MontiSim"
 fi
-cd MontiSim
-git clone --depth 1 "https://github.com/MontiSim/CarLang" "CarLang"
-git clone --depth 1 "https://github.com/MontiSim/server" "server"
-git clone --depth 1 "https://github.com/MontiSim/visualization" "visualization"
-git clone --depth 1 "https://github.com/MontiSim/SimLang" "SimLang"
-git clone --depth 1 "https://github.com/MontiSim/simulation" "simulation"
-git clone --depth 1 "https://github.com/MontiSim/commons" "commons"
-git clone --depth 1 "https://github.com/MontiSim/application" "application"
-git clone --depth 1 "https://github.com/MontiSim/example-autopilot-ema" "example-autopilot-ema"
-git clone --depth 1 "https://github.com/MontiSim/controller" "controller"
-git clone --depth 1 "https://github.com/MontiSim/Documentation" "Documentation"
+cd EmbeddedMontiArc
+for repo in "${msRepos[@]}"
+do
+  if [ -d $repo ]
+  then
+    cd $repo && git pull && cd ..
+  else
+    git clone --depth 1 "https://github.com/MontiSim/$repo"
+  fi
+done
 cd ..
