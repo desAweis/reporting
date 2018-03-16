dbcba440a23d08c25313622daf0211314ab3fd8f
Travis CI <malte.heithoff@rwth-aachen.de>
2018-03-16 02:59:30 +0000

    Deploy Reports to GitHub Pages: ec3c88bf3b1c34e94c22ab169010edfee426700f

--- report/js/testReport.js
+++ report/js/testReport.js
@@ -14,7 +14,7 @@ function loadJSON(file,callback) {
 function createTable(data) {
   data = JSON.parse(data);
   table = $('#my-table').DataTable( {
-      fixedHeader: true,
+      "fixedHeader": true,
       "data" : data,
       "order": [[ 5, "desc" ]],
       "ordering": true,
@@ -32,54 +32,3 @@ function createTable(data) {
       "order": [[1, 'asc']]
   } );      
 };
-  
-/* Formatting function for row details - modify as you need */
-function format ( d ) {
-    // `d` is the original data object for the row
-    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
-        '<tr>'+
-            '<td>Log output:</td>'+
-            '<td>'+d.LogOutput+'</td>'+
-        '</tr>'+
-    '</table>';
-}
-      
-$(document).ready(function() {
-
-  loadJSON("data/dataEWT.json", createTable);
-   
-  // Add event listener for opening and closing details
-  $('#my-table tbody').on('click', 'td.details-control', function () {
-      var tr = $(this).closest('tr');
-      var row = table.row( tr );
-
-      if ( row.child.isShown() ) {
-          // This row is already open - close it
-          row.child.hide();
-          tr.removeClass('shown');
-      }
-      else {
-          // Open this row
-          row.child( format(row.data()) ).show();
-          tr.addClass('shown');
-      }
-  } );
-  
-  $('.grow').mouseover( function(){
-    $(this).find('.shortLabel').hide();
-    $(this).find('.fullLabel').show();
-  });
-  $('.grow').mouseout( function(){
-    $(this).find('.shortLabel').show();
-    $(this).find('.fullLabel').hide();
-  });
-  
-  $('#my-table tbody').on('mouseenter', 'td.grow', function() {
-    $(this).find('.shortLabel').css('display', 'none');
-    $(this).find('.fullLabel').css('display', 'block');
-  } );
-  $('#my-table tbody').on('mouseleave', 'td.grow', function() {
-    $(this).find('.shortLabel').css('display', 'block');
-    $(this).find('.fullLabel').css('display', 'none');
-  } );
-} );
