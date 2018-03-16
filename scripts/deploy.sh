d2ff78af571ee8e9f0d8bd968217e2d5abb4d223
Travis CI <travis@travis-ci.org>
2018-03-16 02:55:29 +0100

    Refactored code

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,41 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+if [ -d "out" ]
+then
+  rm -rf out
+fi
+git clone $REPO out
 cd out
 
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# deploy Zip
+git rm *.zip
+
+for var in "$@"
+do
+    mv ../$var $var
+done
+
+git add *.zip
+git commit -m "Deploy new Zips: ${SHA}"
+
+# deploy reports
 git rm -rf report/css/* || exit 0
 git rm -rf report/data/* || exit 0
 git rm -rf report/images/* || exit 0
 git rm -rf report/js/* || exit 0
-git rm -rf report/cocosReport.html || exit 0
-git rm -rf report/grammarReport.html || exit 0
-git rm -rf report/testReport.html || exit 0
+git rm -rf report/*.html
 if [ ! -d "report" ]
 then
   mkdir report
